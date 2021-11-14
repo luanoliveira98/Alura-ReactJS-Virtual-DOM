d3d5db0d9074f313435cbddf5566394a5f40ee99
Luan <luanoliveira17@hotmail.com>
2021-11-14 20:48:19 -0300

    Using react without import react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import { render } from 'react-dom';
+// import React from 'react';
+// import { render } from 'react-dom';
 
-function App() {
+function convertToHtml(virtualNode) {
+  if (typeof virtualNode === 'string' || typeof virtualNode === 'number') {
+    return document.createTextNode(virtualNode);
+  }
+
+  const $domElement = document.createElement(virtualNode.tagName);
+
+  virtualNode.props.children.forEach((virtualChild) => {
+    $domElement.appendChild(convertToHtml(virtualChild));
+  });
+
+  return $domElement;
+}
+
+function render(initialVirtualTree, $domRoot) {
+  const $appHTML = convertToHtml(initialVirtualTree);
+  $domRoot.appendChild($appHTML);
+}
+
+function createElement(elementType, props, ...children) {
+  const virtualElementProps = {
+    ...props,
+    children
+  }
+
+  if(typeof elementType === "function") {
+    return elementType(props);
+  }
+
+  return {
+    tagName: elementType,
+    props: virtualElementProps
+  };
+}
+
+const React = {
+  createElement,
+}
+
+// ==================================
+
+function App(props) {
   return (
-    <section className="App">
-      <h1>Counter</h1>
-      <div>
-        <div>0</div>
-        <button>Increment</button>
-        <button>Decrement</button>
-      </div>
-    </section>
+    React.createElement("section", {
+      className: "App"
+    }),
+    React.createElement("h1", null, "Counter"),
+    React.createElement("div", null,
+    React.createElement("div", null, "0"),
+    React.createElement("button", null, "Increment"),
+    React.createElement("button", null, "Decrement"))
   );
 }
 
-render(<App/>, document.querySelector('#root'));
+render(React.createElement(App, null), document.querySelector('#root'));
